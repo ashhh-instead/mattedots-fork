36666e92e0ca1657c1420a11164341130fcbb914
Ash <dsksnkz83@gmail.com>
2026-06-03 19:09:46 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,11 +1,8 @@
-bash
-
 sudo
-
 cd .config
 
 git clone https://github.com/dsksnkz/my-hyprland-rice.git
 
-pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish
+pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix 
 
 exit
