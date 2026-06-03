24acf3a09fb802c44a2f874a2dd2cbd4ad8434ab
matte <duskbladez83@gmail.com>
2026-06-03 19:26:17 +0100

    Update installNIRIWM.bash

--- installNIRIWM.bash
+++ installNIRIWM.bash
@@ -1,2 +1,2 @@
 # For installing NIRI WM external (others are still normal)
-sudo cd .config && git clone https://github.com/dsksnkz/my-hyprland-rice.git && pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo Installation either completed or failed. You may now close this Window or report a bug. 
+sudo cd .config && git clone https://github.com/dsksnkz/my-hyprland-rice.git && pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo NIRI - Installation either completed or failed. You may now close this Window or report a bug. 
